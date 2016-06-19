0703a876814f63d0ae8a3910b4ea239953025ee8
Marlon José López Meza <malm.gm@gmail.com>
2016-06-19 13:51:46 -0500

    added copyright header

--- sample/src/main/java/co/marlonlom/google/staticmaps/demo/MainActivity.java
+++ sample/src/main/java/co/marlonlom/google/staticmaps/demo/MainActivity.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2016, marlonlom
+ *
+ * Licensed under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package co.marlonlom.google.staticmaps.demo;
 
 import android.os.Bundle;
@@ -25,6 +42,9 @@ import co.marlonlom.google.staticmaps.StaticMapUrl;
 
 /**
  * The type Main activity.
+ *
+ * @author marlonlom
+ * @version 1.0.0
  */
 public class MainActivity extends AppCompatActivity {
 
